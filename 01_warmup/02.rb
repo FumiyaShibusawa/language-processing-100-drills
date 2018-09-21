b422f8dd7b0968f8c6da31bc136295e0e175b171
FumiyaShibusawa <shizai.asdfghjkl@gmail.com>
2018-09-21 23:53:27 +0900

    update: added some better answers

--- 01_warmup/02.rb
+++ 01_warmup/02.rb
@@ -9,3 +9,7 @@ taxi.length.times do |i|
   output.insert(i * 2 + 1, taxi[i])
 end
 puts output.join("")
+
+# 他の方法
+# $stdout.write ['パトカー'.chars, 'タクシー'.chars].transpose.join
+# 'パトカー'.each_char.zip('タクシー'.each_char).join
