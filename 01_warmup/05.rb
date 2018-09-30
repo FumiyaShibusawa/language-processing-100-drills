0619799b806daab7edc92ebfbc2724e54c84879c
FumiyaShibusawa <shizai.asdfghjkl@gmail.com>
2018-09-30 23:48:08 +0900

    update: added bi-gram

--- 01_warmup/05.rb
+++ 01_warmup/05.rb
@@ -1,3 +1,15 @@
 # 05. n-gram
 # 与えられたシーケンス（文字列やリストなど）からn-gramを作る関数を作成せよ．この関数を用い，
-# "I am an NLPer"という文から単語bi-gram，文字bi-gramを得よ．
+# 'I am an NLPer'という文から単語bi-gram，文字bi-gramを得よ．
+
+# 単語bi-gram
+word_bi_gram = []
+'I am an NLPer'.split.each_cons(2) { |s| word_bi_gram << s }
+
+# 文字bi-gram
+letter_bi_gram = []
+# こっちの書き方だと冗長かも → 'I am an NLPer'.split('').map { |a| a.delete(' ') }.reject(&:empty?)
+'I am an NLPer'.delete(' ').split('').each_cons(2) { |l| letter_bi_gram << l }
+
+puts "word_bi_gram: #{word_bi_gram}"
+puts "letter_bi_gram: #{letter_bi_gram}"
