666d7fa87ef6272c590eaad8296d3b67a1eba148
FumiyaShibusawa <shizai.asdfghjkl@gmail.com>
2018-10-09 15:25:32 +0900

    update: added yard info for the method.

--- 01_warmup/09.rb
+++ 01_warmup/09.rb
@@ -3,10 +3,19 @@
 # ただし，長さが４以下の単語は並び替えないこととする．適当な英語の文（例えば"I couldn't believe that I could actually
 # understand what I was reading : the phenomenal power of the human mind ."）を与え，その実行結果を確認せよ．
 
+# Shuffles all the letters except the first/last one of
+# words which has more than 4 letters in a sentence.
+#
+# @param [<String>] a sentence which has a series of words.
+# @return [<String>] a new sentence with the words shuffled.
+#
+def shuffle_letters_in(sentence)
+  sentence.split.map do |word|
+    word[1..-2] = word[1..-2].split('').shuffle.join if word.length > 4
+    word
+  end.join(' ')
+end
+
 sentence = "I couldn't believe that I could actually understand what I was \
 reading : the phenomenal power of the human mind ."
-
-sentence.split.map do |word|
-  word[1..-2] = word[1..-2].split('').shuffle.join if word.length > 4
-  word
-end
+puts shuffle_letters_in(sentence)
