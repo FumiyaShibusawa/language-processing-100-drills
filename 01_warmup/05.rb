73578907fc35490903296c7ec513b234f6feacab
FumiyaShibusawa <shizai.asdfghjkl@gmail.com>
2018-10-10 03:07:02 +0900

    update: added appropriate methods and yard.

--- 01_warmup/05.rb
+++ 01_warmup/05.rb
@@ -2,14 +2,41 @@
 # 与えられたシーケンス（文字列やリストなど）からn-gramを作る関数を作成せよ．この関数を用い，
 # 'I am an NLPer'という文から単語bi-gram，文字bi-gramを得よ．
 
+#
+# Returns n-gram word by word
+#
+# @param [<String>] sentence <to be n-grammed>
+# @param [<Integer>] n_gram <n as in n-gram to be assigned>
+#
+# @return [<Array>] <pair of n-gram word by word>
+#
+def create_word_n_gram(sentence, n_gram)
+  words = sentence.split
+  words.each_cons(n_gram).with_object([]) do |word, array|
+    array << word
+  end
+end
+
+#
+# Returns n-gram letter by letter
+#
+# @param [<String>] sentence <to be n-grammed>
+# @param [<Integer>] n_gram <n as in n-gram to be assigned>
+#
+# @return [<Array>] <pair of n-gram letter by letter>
+#
+def create_letter_n_gram(sentence, n_gram)
+  letters = sentence.delete(' ')
+  letters.split('').each_cons(n_gram).with_object([]) do |letter, array|
+    array << letter
+  end
+end
+
 # 単語bi-gram
-word_bi_gram = []
-'I am an NLPer'.split.each_cons(2) { |s| word_bi_gram << s }
+word_bi_gram = create_word_n_gram('I am an NLPer', 2)
 
 # 文字bi-gram
-letter_bi_gram = []
-# こっちの書き方だと冗長かも → 'I am an NLPer'.split('').map { |a| a.delete(' ') }.reject(&:empty?)
-'I am an NLPer'.delete(' ').split('').each_cons(2) { |l| letter_bi_gram << l }
+letter_bi_gram = create_letter_n_gram('I am an NLPer', 2)
 
 puts "word_bi_gram: #{word_bi_gram}"
 puts "letter_bi_gram: #{letter_bi_gram}"
